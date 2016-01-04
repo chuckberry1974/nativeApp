620d59a7b3ca7992d9a246700137396b2bc1f2f7
chuckberry1974 <steve.tan1503@gmail.com>
2016-01-04 14:27:37 +0800

    change main.js

--- Public/main.js
+++ Public/main.js
@@ -1,10 +1,7 @@
 var navStandAlone = function standalone () {
-  if (window.navigator.standalone) {
-// return 'NAVIGATOR IS WORKING!!!'
-    document.querySelector('body').textcontent = 'SHIT'
-  } else {
-    document.querySelector('body').textcontent = 'NOT SHIT'
-// return 'not working'
-  }
+
+
+
 }
+
 navStandAlone()
